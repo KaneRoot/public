15364b16e96be3363b7b9aa5670d1f48761dd5c7
Philippe Pittoli <kane.root@gmail.com>
2011-09-13 12:58:00 +0200

    définition d'un arbre binaire

--- Licence2_S4/SDA2/arbres-binaires/abchain.c
+++ Licence2_S4/SDA2/arbres-binaires/abchain.c
@@ -2,7 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct arbin
+{
+	struct SARBIN *g;
+	int s;
+	struct SARBIN *d;
+} SARBIN;
+typedef SARBIN * sarbin;
+
 void main(int argc, char * argv[])
 {
+	sarbin truc = (sarbin) malloc(sizeof(sarbin));
+	truc->s = 15;
+	printf("%d ;\n",truc->s);
 	exit(EXIT_SUCCESS);
 }
