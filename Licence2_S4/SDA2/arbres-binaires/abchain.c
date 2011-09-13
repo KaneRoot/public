0c902cce261a2c7af608e6514ecc749a997abb57
Philippe Pittoli <kane.root@gmail.com>
2011-09-13 23:16:35 +0200

    Création d'arbre trié fonctionnel

--- Licence2_S4/SDA2/arbres-binaires/abchain.c
+++ Licence2_S4/SDA2/arbres-binaires/abchain.c
@@ -1,3 +1,9 @@
+/*
+ * arbin.c
+ *
+ * 13/09/11
+ *
+ */
 #include "abchain.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,8 +11,20 @@
 
 int main(int argc, char * argv[])
 {
-	SARBIN * truc = creation_arbin();
-	printf("%d ;\n", truc->s);
+	int i = 0;
+	int *x = (int *) malloc(sizeof(int*)*10);
+	for(i = 0 ; i < 9 ; i++)
+		*(x + i) = i+ 10;
+	x[9] = NULL;
+	SARBIN * truc;
+	//printf("%d \n", truc->s);
+	fprintf(stdout, "Création de l'arbre\n");
+	fprintf(stdout, "-------------------\n");
+	truc = creation_arbre_trie(x);
+	fprintf(stdout, "Parcours de l'arbre\n");
+	fprintf(stdout, "-------------------\n");
+	infixe(truc, ecrire);
+
 	return EXIT_SUCCESS;
 }
 
@@ -36,11 +54,40 @@ bool v(SARBIN *a)
 {
 	return (a == NULL) ? VRAI : FAUX;
 }
-SARBIN * creation_arbin()
+SARBIN * creation_noeud()
 {
 	SARBIN * a = (SARBIN *) malloc(sizeof(SARBIN));
 	a->s = -1;
 	a->g = NULL;
 	a->d = NULL;
 	return a;
+}
+void ecrire(int x)
+{
+	printf(" %d \n", x);
+}
+SARBIN * creation_arbre_trie(int *x)
+{
+	SARBIN * tmp = creation_noeud();
+	int i;
+	for(i = 0 ; *(x + i) != NULL ; i++)
+	{
+		tmp = insr(tmp,*(x + i));
+	}
+	return tmp;
+}
+SARBIN * insr(SARBIN * a, int x)
+{
+	SARBIN * res = a;
+	if( v(res) == VRAI )
+	{
+		res = creation_noeud();
+		res->s = x;
+	}
+	else if(res->s > x)
+		 res->g = insr(ag(res),x);
+	else
+		 res->d = insr(ad(res),x);
+
+	return res;
 }
---
+++ Licence2_S4/SDA2/arbres-binaires/abchain.h
@@ -0,0 +1,23 @@
+/* 
+ *	abchain.h
+ *	
+ *	13/09/11
+ */
+
+typedef enum { FAUX, VRAI } bool;
+typedef struct arbin
+{
+	struct SARBIN *g;
+	int s;
+	struct SARBIN *d;
+} SARBIN;
+
+void infixe(SARBIN * a, void (*pr)(int));
+SARBIN * ag(SARBIN *);
+SARBIN * ad(SARBIN *);
+int r(SARBIN *);
+bool v(SARBIN *);
+SARBIN * creation_noeud(void);
+void ecrire(int);
+SARBIN * creation_arbre_trie(int*);
+SARBIN * insr(SARBIN *,int);
