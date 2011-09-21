4add8a25491f5bb5e1de8136d29092f67653d09e
Philippe Pittoli <kane.root@gmail.com>
2011-09-21 15:30:46 +0200

    derniers changements

--- Licence2_S4/PSR/TP1/tp1.c
+++ Licence2_S4/PSR/TP1/tp1.c
@@ -1,24 +1,78 @@
 #include "tp1.h"
 #include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-#define MAX_BUF1 10
+#define MAXBUF_GETCHAR2 10 // pour getchar2 
 
 int main(int argc, char * argv[])
 {
 	//printf("EOF : %d\n",EOF);
-	char c = (char) getchar();
-	printf("%c\n",c);
+	//test_getchar1();
+	//test_getchar2();
+	mystat(argc,argv);
 	return EXIT_SUCCESS;
 }
-int getchar1(void)
+void test_getchar2()
 {
-	char c;
-	read(0, &c, 1);
-	return (int) c;
+	int c;
+	while((c = getchar2()) != EOF)
+		putchar(c);
 }
 int getchar2(void)
 {
-	return 0;
+	static char buf[MAXBUF_GETCHAR2];
+	static int indice = 0;
+	static int taille = 0;
+	if(indice == 0 || indice >= (taille ))
+	{
+		taille = read(0, buf, MAXBUF_GETCHAR2);
+		if(taille == 0)
+			buf[0] = EOF;
+		indice = 0;
+	}
+	indice++;
+	return (int) buf[indice - 1];
+}
+void test_getchar1()
+{
+	int c;
+	while((c = getchar1()) != EOF)
+		putchar(c);
+}
+int getchar1(void)
+{
+	int n;
+	char c;
+	n = read(0, &c, 1);
+	if(n == 1)
+		return (int) c;
+	return EOF;
+}
+void mystat(int argc, char * argv[])
+{
+	if(argc != 2)
+	{
+		fprintf(stderr, "USAGE : %s file\n",argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	int e;
+	struct stat stat_buf;
+	if((stat(argv[1],&stat_buf)) == -1)
+	{
+		perror("");
+		exit(EXIT_FAILURE);
+	}
+	switch(stat_buf.st_mode)
+	{
+		case S_ISREG :
+			printf("Ce fichier est ordinaire\n");
+		break;
+		default :
+			printf("RIEN A DIRE \n");
+		break;
+	}
+
 }
--- Licence2_S4/PSR/TP1/tp1.h
+++ Licence2_S4/PSR/TP1/tp1.h
@@ -1,2 +1,5 @@
 int getchar1(void);
 int getchar2(void);
+void test_getchar1();
+void test_getchar2();
+void mystat(int,char**);
