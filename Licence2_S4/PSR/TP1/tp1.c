9bb38fce8644d1d79359ce77c830f10255eb6541
Philippe Pittoli <kane.root@gmail.com>
2011-09-21 19:03:11 +0200

    std=c99 ne fonctionne pas, remplacement en std=c90

--- Licence2_S4/PSR/TP1/tp1.c
+++ Licence2_S4/PSR/TP1/tp1.c
@@ -1,11 +1,13 @@
 #include "tp1.h"
-#include <stdio.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define MAXBUF_GETCHAR2 10 // pour getchar2 
+#define __USE_GNU
 
 int main(int argc, char * argv[])
 {
@@ -58,21 +60,24 @@ void mystat(int argc, char * argv[])
 		fprintf(stderr, "USAGE : %s file\n",argv[0]);
 		exit(EXIT_FAILURE);
 	}
-	int e;
 	struct stat stat_buf;
 	if((stat(argv[1],&stat_buf)) == -1)
 	{
-		perror("");
+		perror("stat");
 		exit(EXIT_FAILURE);
 	}
-	switch(stat_buf.st_mode)
+	/*
+	switch (stat_buf.st_mode & S_IFMT) 
 	{
-		case S_ISREG :
-			printf("Ce fichier est ordinaire\n");
-		break;
-		default :
-			printf("RIEN A DIRE \n");
-		break;
+		case S_IFBLK:  printf("block device\n");            break;
+		case S_IFCHR:  printf("character device\n");        break;
+		case S_IFDIR:  printf("directory\n");               break;
+		case S_IFIFO:  printf("FIFO/pipe\n");               break;
+		case S_IFLNK:  printf("symlink\n");                 break;
+		case S_IFREG:  printf("regular file\n");            break;
+		case S_IFSOCK: printf("socket\n");                  break;
+		default:       printf("unknown?\n");                break;
 	}
-
+	*/
+	printf("\n");
 }
