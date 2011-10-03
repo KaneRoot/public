1fe5d3cc6c7e2f31b5a5c003e0d37279786a330d
Philippe Pittoli <kane.root@gmail.com>
2011-10-03 15:24:57 +0200

    TP1 terminé, presque

--- licence_s4/poo/tp1/Moyenne.java
+++ licence_s4/poo/tp1/Moyenne.java
@@ -3,10 +3,37 @@ public class Moyenne
 	public static void main(String[] args)
 	{
 		int i, somme = 0;
+		int minimum = Integer.parseInt(args[0]);
+		int maximum = Integer.parseInt(args[0]);
+
 		for(i = 0 ; i < args.length ; i++)
 			somme += Integer.parseInt(args[i]);
 
 		System.out.println("La somme est : " + somme);
 		System.out.println("La moyenne est : " + (somme / args.length));
+		for(i = 1 ; i < args.length ; i++)
+			minimum = min(minimum,Integer.parseInt(args[i]));
+		for(i = 1 ; i < args.length ; i++)
+			maximum = max(maximum,Integer.parseInt(args[i]));
+
+		System.out.println("Le minimum : " + minimum);
+		System.out.println("Le maximum : " + maximum);
+
+	}
+	public static int min(int a, int b)
+	{
+		return (a < b) ? a : b;
+	}
+	public static int max(int a, int b)
+	{
+		return (a > b) ? a : b;
+	}
+	public static Integer min(Integer a, Integer b)
+	{
+		return (a.compareTo(b) <= 0) a : b;
+	}
+	public static Integer max(Integer a, Integer b)
+	{
+		return (a.compareTo(b) >= 0) a : b;
 	}
 }
