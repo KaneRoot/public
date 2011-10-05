48e233ba804869607d7624ac3eb88a056f41c301
Philippe Pittoli <kane.root@gmail.com>
2011-10-05 17:31:07 +0200

    ajout du crible d'ératosthène non terminé

--- licence_s4/psr/tp2/tp2.c
+++ licence_s4/psr/tp2/tp2.c
@@ -41,26 +41,48 @@ void max_valeur_tube()
 	while((write(p[1],c,1)))
 		printf("Val max à rentrer dans le pipe %d\n",somme++);
 }
-void crible_eratosthene(int n)
-{
-	int t[2], tprec[2],i;
-	for(i = 0 ; i < n ; i++)
-	{
-		pipe(t);
-		if(fork())
-		{
-
-			printf("FILS\n");
-			return;
-		}
-	}
-}
 void copie(int a, int b)
 {
 	int n;
-	char * c = malloc(sizeof(char) * 1);
+	char c[1];
 	while((n = read(a,c,1)))
 		write(b,c,n);
+}
+// Pas fini
+void crible_eratosthene(int n)
+{
+	int t[2], tprec[2],i,pid;
+	for(i = 0 ; i < n ; i++)
+	{
+		pipe(t);
+		if((pid =fork()))
+		{
+			if(i == 0)
+			{
+				close(1); close(t[0]);
+				copie(0,t[1]);
+				close(0); close(t[1]);
+				exit(EXIT_SUCCESS);
+			}
+			else
+			{
+				close(t[0]); close(tprec[1]);
+				copie(tprec[0],t[1]);
+				close(tprec[0]); close(t[1]);
+				exit(EXIT_SUCCESS);
+			}
+		}
+		else
+		{
+			close(t[1]);
+			tprec[0] = t[0];
+		}
+	}
+	copie(t[0],1);
+	close(t[0]);
+	close(1);
+	for(i = 0 ; i < n ; i++)
+		wait(NULL);
 }
 int main(int argc, char * argv[])
 {
