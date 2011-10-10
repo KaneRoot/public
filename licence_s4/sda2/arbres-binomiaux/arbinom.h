8e87cc951d3e82e8ceff8456a155a3e749a4e5e3
Philippe Pittoli <kane.root@gmail.com>
2011-10-10 13:46:53 +0200

    Changement de place des déclarations de types

--- licence_s4/sda2/arbres-binomiaux/arbinom.h
+++ licence_s4/sda2/arbres-binomiaux/arbinom.h
@@ -1,12 +1,6 @@
 #ifndef ARBINOM
 #define ARBINOM
 
-/* definitions */
-typedef unsigned int Nat;
-typedef int Ent;
-typedef float Rat;
-typedef enum { FAUX = 0, VRAI = 1 } bool;
-
 /* structures */
 typedef struct arbinom
 {
