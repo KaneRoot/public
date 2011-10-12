01087f3b166eb0fdad32e97ab00e60707897945d
Philippe Pittoli <kane.root@gmail.com>
2011-10-12 16:23:53 +0200

    Bientôt fini l'exo2

---
+++ licence_s4/psr/tp3/tp3_exo2.c
@@ -0,0 +1,52 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(int argc, char * argv[])
+{
+	int palpha[2],pdigit[2],nb_chiffres = 0, nb_lettres = 0;
+	char c;
+	char buf[1];
+
+	// premier fils, alpha
+	pipe(palpha);
+	if(fork() == 0)
+	{
+		close(0); 
+		close(palpha[1]);
+		while((read(palpha[0],buf,1)))
+		{
+			nb_lettres++;
+		}
+		printf("nb de lettres = %d\n", nb_lettres);
+		exit(EXIT_SUCCESS);
+	}
+	close(palpha[0]);
+
+	// second fils, digit
+	pipe(pdigit);
+	if(fork() == 0)
+	{
+		close(0); 
+		close(palpha[1]);
+		close(pdigit[1]);
+		while((read(pdigit[0],buf,1)))
+		{
+			nb_chiffres++;
+		}
+		printf("nb de chiffres = %d\n", nb_chiffres);
+		exit(EXIT_SUCCESS);
+	}
+	while((c = getchar()) != EOF)
+	{
+		buf[0] = c;
+		if(isalpha(c)) write(palpha[1],buf,1);
+		if(isdigit(c)) write(pdigit[1],buf,1);
+	}
+	close(palpha[1]);
+	close(pdigit[1]);
+	return EXIT_SUCCESS;
+}
+
