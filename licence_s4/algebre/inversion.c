1c1f7e93cdeada168641773fa321152907fbedab
Philippe Pittoli <kane.root@gmail.com>
2011-11-06 17:43:35 +0100

    voilà

--- licence_s4/algebre/inversion.c
+++ licence_s4/algebre/inversion.c
@@ -12,3 +12,10 @@ matrice_s * inversion_gauss_jordan(matrice_s * m)
 
 	return inverse;
 }
+matrice_s * inversion_cofacteurs(matrice_s * m)
+{
+	matrice_s * inverse;
+	inverse = create_matrix(m->nbl, m->nbc);
+
+	return inverse;
+}
--- licence_s4/algebre/inversion.h
+++ licence_s4/algebre/inversion.h
@@ -1,4 +1,7 @@
 #ifndef INVERSION
 #define INVERSION
 
+matrice_s * inversion_gauss_jordan(matrice_s * m);
+matrice_s * inversion_cofacteurs(matrice_s * m);
+
 #endif
