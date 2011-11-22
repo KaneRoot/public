066d38803c684899893cac42dfb2afb8be377aba
Dijook <justino27@hotmail.com>
2011-11-22 15:32:55 +0100

    J'ai mis mes changements là-dedans

--- licence_s4/arch/mips/autre.s
+++ licence_s4/arch/mips/autre.s
@@ -1,122 +1,28 @@
-
-# Programme en MIPS
-
-# Interpréteur : SPIM
-# spim -notrap -file programme.s
-
-.data
-	val1:	.asciiz	"Entrez la valeur 1 : "
-	val2:	.asciiz	"Entrez la valeur 2 : "
-	str_space_bar:	.asciiz " | "
-	res:	.asciiz "Le résultat est : "
-	str_fin:	.asciiz "Fin du programme\n"
-	str_endl:	.asciiz "\n"
-
-.text
-.globl __start
-
-__start:	li $v0, 4
-			la $a0, val1
-			syscall
-			
-			jal get_int
-			move $t0, $a0
-
-label2:		li $v0, 4
-			la $a0, val2
-			syscall
-
-			jal get_int
-			move $t1, $a0
-			jal write_int_space
-
-afficheres:	li $v0, 4
-			la $a0, res
-			syscall
-			add $t2, $t1,$t0
-			li $v0, 1
-			move $a0, $t2
-			syscall
-			li $v0, 4
-			la $a0, str_endl
-			syscall
-			jal truc
-fin:	
-	la $a0, str_fin				# chargement de la chaîne str_fin
-	jal write_string			# écriture de 'fin du programme'
-	li $v0, 10					# appel système n. 10
-	syscall						# fin du programme
-
-# affiche une valeur, fonction de type stem
-write_int_nl:	
-	li $v0, 1					# appel système n. 1
-	syscall						# lit un entier dans a0
-	move $t4, $ra				# chargement de ra dans t4 pour sauvegarde
-	jal write_nl				# écriture d'un retour à la ligne
-	move $ra, $t4				# replacement de l'ancienne valeur de ra
-	j $ra						# retour à l'instruction appelante
-
-# écrit un retour à la ligne, fonction de type leaf
-write_nl:	
-	li $v0, 4					# appel système n. 4
-	la $a0, str_endl			# chargement d'une chaîne (\n)
-	syscall						# écriture de la chaîne
-	j $ra						# retour à l'instruction appelante
-
-write_int_space:	
-	sub $sp, $sp, 4
-	sw $ra, ($sp)
-	li $v0, 1					# appel système n. 1
-	syscall						# lit un entier dans a0
-	la $a0, str_space_bar		# chargement de l'@ de str_space_bar
-	jal write_string			# écriture de la chaîne
-	lw $ra, ($sp)
-	addu $sp, $sp, 4
-	j $ra						# retour à l'instruction appelante
-
-write_string:		
-	li $v0, 4					# appel système n. 4
-	syscall						# écriture de la chaîne
-	j $ra						# retour à l'instruction appelante
-
-write_space_bar:
-	sub $sp, $sp, 4
-	sw $ra, ($sp)
-	la $a0, str_space_bar
-	jal write_string
-	lw $ra, ($sp)
-	addu $sp, $sp, 4
-	j $ra
-truc:
-	sub $sp, $sp, 4
-	sw $ra, ($sp)
-	jal truc2
-	lw $ra, ($sp)
-	add $sp, 4
-	j $ra
-truc2:
-	sub $sp, $sp, 4
-	sw $ra, ($sp)
-	jal truc3
-	lw $ra, ($sp)
-	add $sp, 4
-	j $ra
-
-truc3:
-	sub $sp, $sp, 4
-	sw $ra, ($sp)
-	li $a0, 15
-	jal write_int_nl
-	lw $ra, ($sp)
-	add $sp, 4
-	j $ra
-
-get_int:
-	sub $sp, $sp, 4
-	sw $ra, ($sp)
-	li $v0, 5					# appel système n. 5
-	syscall						# on lit un entier
-	move $a0, $v0				# on met la réponse dans a0
-	lw $ra, ($sp)
-	addu $sp, $sp, 4
+patern_detector_line:
+	sub $sp, $sp, 4				# soustrait 4 au pointeur de pile
+	sw $ra, ($sp)				# sauvegarde ra dans la pile
+	li $t0, $s0					# initialise le pointeur du tableau
+	li $t1, 0					# initialise increment à 0
+	li $t2, 0					# initialise valeur du tableau à 0
+	li $t3, 0					# initialise numero ligne à 0
+	li $t4, 4					# va servir pour le test increment != 4
+	
+patern_detector_line_loop:	
+	lw $t2, ($t0)								# charge la valeur du tableau dans t2
+	bne $t2, $s1, patern_detector_line_then		# test si t2 != numéro du joueur
+	add $t1, $t1, 1								# t1 += 1
+	bne $t1, $t4, patern_detector_line_loop		# test si increment != 4 
+	move $s2, $t2								# si t1 = 4 alors on place le numéro du joueur ($t2) dans le numéro du gagnant ($s2)
+	j patern_detector_line_end
+patern_detector_line_then:
+	add $t0, $t0, 1								# t0 += t0
+	j patern_detector_line_end
+	
+	add $t3, $t3, 1								# t3 += 1
+	bgt $s4, $t3, patern_detector_line_loop		# test si numero ligne > nb ligne
+	j patern_detector_line_end
+	
+patern_detector_line_end:	
+	lw $ra, ($sp)				# charge ra depuis la pile
+	addu $sp, $sp, 4			# ajoute 4 au pointeur de pile
 	j $ra						# retour à l'instruction appelante
