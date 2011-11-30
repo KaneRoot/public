6420756854ce82347282684db3c0f0198737acd3
Philippe Pittoli <kane.root@gmail.com>
2011-11-30 17:00:28 +0100

    voilà

--- licence_s4/psr/tp5/emetteur_skel.c
+++ licence_s4/psr/tp5/emetteur_skel.c
@@ -1,4 +1,4 @@
-
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -17,22 +17,16 @@ int main (int argc, char *argv[])
 	struct sockaddr_in  addr_recepteur; // structure d'adresse du recepteur
  	int size_addr_in = sizeof(struct sockaddr_in); // utile pour sendto	
 	int aenvoyer = atoi(argv[3]);
-
     int s; // socket
-
-
 	if (argc!=4)
 	{
 		fprintf(stderr, "Usage : %s @recepteur port_recepteur message\n",argv[0]);
 		exit (1);
 	}
-
 	// création socket
-
 	s = socket(AF_INET,SOCK_DGRAM,0);
 
   	// adresse recepteur	
-
 	memset(&hints,0,sizeof hints);
 	hints.ai_socktype = SOCK_DGRAM; // UDP
 	hints.ai_family = AF_INET; // IPv4
@@ -47,8 +41,7 @@ int main (int argc, char *argv[])
   	addr_recepteur.sin_port=htons(atoi(argv[2]));
   	addr_recepteur.sin_addr=((struct sockaddr_in *)resultat->ai_addr)->sin_addr;
 
-	
-	if(sendto(s,&aenvoyer,sizeof(int),0,(struct sockaddr*)&addr_recepteur,size_addr_in) == -1)
+	if(sendto(s,&aenvoyer,sizeof aenvoyer ,0,(struct sockaddr*)&addr_recepteur,size_addr_in) == -1)
  	{
 		fprintf(stderr, "erreur sendto\n");
 		exit(1);
