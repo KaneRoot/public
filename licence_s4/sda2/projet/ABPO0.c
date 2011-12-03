366a94e0b9f63242c105e4a5746052bbcccd795a
Philippe Pittoli <kane.root@gmail.com>
2011-12-04 00:55:59 +0100

    fin de l'exo b, sauf pour les tests

--- licence_s4/sda2/projet/ABPO0.c
+++ licence_s4/sda2/projet/ABPO0.c
@@ -13,11 +13,26 @@ bool po(Abp a)
 	//return v(a,n(a)-1) >= v(inser(a,s),mount(a,n(a
 	int i;
 	bool b = VRAI;
-	for(i = 0 ; i < n(a) || b ; i++)
-		if(v(a,i) < mount(a,i))
+	for(i = 0 ; i < n(a) && b ; i++)
+		if(v(a,i) < v(a,mont(a,i)))
 			b = FAUX;
 
 	return b;
 }
-//Abp ipo(Abp, Rat);
-//Nat mont(Abp, Nat);
+Nat mont(Abp a, Nat x)
+{
+	if(x==0) return 0;
+	return ((x%2)==0) ? (int) (x-1)/2 : (int) x/2;
+}
+Abp ipo(Abp a, Rat s)
+{
+	if(n(a) <= 0) return inser(a,s);
+	if(s < v(a,mont(a,n(a))))
+	{
+		a = inser(a,s);
+		a = ech(a,mont(a,n(a)-1),n(a)-1);
+	}
+	else
+		a = inser(a,s);
+	return a;
+}
