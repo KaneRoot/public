0ba275476c1dd432cd941f76a8e482d0920112f8
Philippe Pittoli <kane.root@gmail.com>
2011-12-03 17:31:46 +0100

    Fin de la partie a, sans tests

--- licence_s4/sda2/projet/ABP.c
+++ licence_s4/sda2/projet/ABP.c
@@ -5,6 +5,8 @@
 #include "base.h"
 #include "ABP.h"
 
+#define V_ERROR -13.37
+
 Abp lambda(void)
 {
 	Abp a = malloc(sizeof(Abp));
@@ -34,6 +36,7 @@ Abp r(Abp a, Nat x, Rat s)
 }
 Abp ech(Abp a, Nat x, Nat y)
 {
+	return (x >= n(a) || y >= n(a)) ? a : r(r(a,x,v,(a,y)),y,v(a,x));
 }
 Nat nf(Abp a)
 {
@@ -41,7 +44,12 @@ Nat nf(Abp a)
 	int arrondi = (int) n(a)/2;
 	return (Nat) n(a) - arrondi;
 }
-Rat v(Abp,Nat);
+Rat v(Abp a, Nat x)
+{
+	if(x >= n(a))
+		return V_ERROR;
+	return a->t[x];
+}
 Nat n(Abp a)
 {
 	if(a == NULL) return 0; // Sécurité contre les segfaults.
