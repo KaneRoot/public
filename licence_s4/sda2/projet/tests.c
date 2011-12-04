af15c099581ea3153312141164a1689a5f9ce269
Philippe Pittoli <kane.root@gmail.com>
2011-12-04 23:32:05 +0100

    ajout de tests

--- licence_s4/sda2/projet/tests.c
+++ licence_s4/sda2/projet/tests.c
@@ -7,13 +7,20 @@
 #include "ABPO0.h"
 #include "ABPO1.h"
 
+void afficher_nb_noeuds(Abp a)
+{
+	printf("	Nombre de nœuds : %d et de feuilles : %d\n",n(a), nf(a));
+}
 void afficher_arbre(Abp a)
 {
 	int i;
 	printf("\033[31m[\033[00m ");
 	for(i = 0 ; i < n(a)-1 ; i++)
 		printf("%2.1f | ",v(a,i));
-	printf("%2.1f \033[31m]\033[00m\n",v(a,n(a)-1));
+	if(n(a) > 0) 
+		printf("%2.1f \033[31m]\033[00m\n",v(a,n(a)-1));
+	else
+		printf("\033[31m]\033[00m\n");
 }
 
 void afficher_po(Abp a)
@@ -27,41 +34,42 @@ void afficher_po(Abp a)
 
 void ajout_suppression_echange()
 {
-	printf("\033[31mTest : ajout_suppression_echange\033[00m\n");
+	printf("\n\033[31mTest : ajout_suppression_echange\033[00m\n");
+	printf("	Ajout de 6 valeurs à un arbre a\n");
 	int i;
 	Abp a = lambda();
-	printf("Ajout de 6 valeurs à un arbre a\n");
 	for(i = 0 ; i < 6 ; i++)
 		inser(a,i);
 	afficher_arbre(a);
-	printf("Suppression de 2 éléments dans l'arbre a\n");
+	printf("	Suppression de 2 éléments dans l'arbre a\n");
 	for(i = 0 ; i < 2 ; i++)
 		s(a);
 	afficher_arbre(a);
-	printf("Inversion de 2 éléments dans a : ech(a, 2, 3)\n");
+	printf("	Inversion de 2 éléments dans a : ech(a, 2, 3)\n");
 	ech(a,2,3);
 	afficher_arbre(a);
 }
 void nb_noeuds_feuilles()
 {
-	printf("\033[31mTest : nb_noeuds_feuilles\033[00m\n");
+	printf("\n\033[31mTest : nb_noeuds_feuilles\033[00m\n");
+	printf("	Ajout de 6 éléments à un arbre a\n");
 	int i;
 	Abp a = lambda();
 	for(i = 0 ; i < 6 ; i++)
 		inser(a,i);
-
-	printf("Nombre de nœuds : %d et de feuilles : %d\n",n(a), nf(a));
+	afficher_arbre(a);
+	afficher_nb_noeuds(a);
 	printf("Suppression d'un élément\n");
 	s(a);
-	printf("Nombre de nœuds : %d et de feuilles : %d\n",n(a), nf(a));
+	afficher_nb_noeuds(a);
 	printf("Suppression d'un élément\n");
 	s(a);
-	printf("Nombre de nœuds : %d et de feuilles : %d\n",n(a), nf(a));
+	afficher_nb_noeuds(a);
 }
 void po_ipo_mont()
 {
-	printf("\033[31mTest : po_ipo_mont\033[00m\n");
-	printf("	1 arbre 4 ajouts de 0 à 3, test po, puis ajouts de 0 et test po\n");
+	printf("\n\033[31mTest : po_ipo_mont\033[00m\n");
+	printf("	Un arbre 'a' auquel je fais 4 ajouts de 0 à 3, test po, puis ajout de 0  via 'i' et test po.\n");
 	Abp a = lambda();
 	int i;
 	for(i = 0 ; i < 4 ; i++)
@@ -71,17 +79,19 @@ void po_ipo_mont()
 	inser(a,0);
 	afficher_arbre(a);
 	afficher_po(a);
-	printf("	1 arbre 10 ajouts de 10 à 1 via ipo, test po\n"); //, puis ipo de 0 et test po\n");
+	printf("\n	Un arbre 'b' auquel je fais 10 ajouts de 10 à 1 via 'ipo' puis test po.\n");
 	Abp b = lambda();
 	for(i = 0 ; i < 10 ; i++)
 		ipo(b, 10 - i);
 	afficher_arbre(b);
 	afficher_po(b);
+	printf("	Test v(a,imont(2)) : %2.1f\n",v(a,imont(2)));
+	printf("	Test v(a,imont(6)) : %2.1f\n",v(a,imont(6)));
 }
 void spo_desc_imin()
 {
-	printf("\033[31mTest : spo_desc_imin\033[00m\n");
-	printf("Ajout à l'arbre a de 10 éléments de 10 à 1\n");
+	printf("\n\033[31mTest : spo_desc_imin\033[00m\n");
+	printf("	Un arbre 'a' de 10 éléments de 10 à 1 via ipo\n");
 	Abp a = lambda();
 	int i;
 	for(i = 0 ; i < 10 ; i++) ipo(a,10-i);
@@ -91,20 +101,24 @@ void spo_desc_imin()
 	spo(a,3);
 	afficher_arbre(a);
 	afficher_po(a);
-	printf("Nombre de nœuds : %d et de feuilles : %d\n",n(a), nf(a));
-	printf("Ajout de l'élément -1 puis suppression de l'élément 2\n");
+	afficher_nb_noeuds(a);
+	printf("Ajout de l'élément -1\n");
 	ipo(a,-1);
+	afficher_arbre(a);
+	printf("Suppression de l'élément 2\n");
 	spo(a,2);
 	afficher_arbre(a);
 	afficher_po(a);
-
+	printf("	Test v(a,imin(a,2)) : %2.1f\n",v(a,imin(a,2)));
+	printf("	Test v(a,imin(a,6)) : %2.1f\n",v(a,imin(a,6)));
 }
 int main(int argc, char * argv[])
 {
-	//ajout_suppression_echange();
-	//nb_noeuds_feuilles();
-	//po_ipo_mont();
+	ajout_suppression_echange();
+	nb_noeuds_feuilles();
+	po_ipo_mont();
 	spo_desc_imin();
+
 	return EXIT_SUCCESS;
 }
 
