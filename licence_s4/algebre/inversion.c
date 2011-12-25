7721af94d73bbc464ecc195f9f058c37e5db2e57
Philippe Pittoli <kane.root@gmail.com>
2011-12-25 17:58:24 +0100

    Ajout de la fonction d'inversion via gauss, semble fonctionner

--- licence_s4/algebre/inversion.c
+++ licence_s4/algebre/inversion.c
@@ -6,12 +6,48 @@
 #include "determinant.h"
 #include "comatrice.h"
 #include "calculs_base.h"
+#include "inversion.h"
 
 // TODO
-matrice_s * inversion_gauss_jordan(matrice_s * m)
+matrice_s * inversion_gauss_jordan(matrice_s * m1)
 {
-	matrice_s * inverse;
-	inverse = create_matrix(m->nbl, m->nbc);
+	int i, j, l_pivot = 0, c_pivot = 0;
+	float coeff;
+	matrice_s * inverse, * m;
+	inverse = matrice_identitee(m1->nbc);
+	m = dupliquer_matrice(m1);
+
+	for(i = 0 ; i < m->nbl ; i++)
+	{
+		if((l_pivot = recherche_ligne_pivot_suivant(m, i, &c_pivot)) >= 0)
+		{
+			if(i != l_pivot)
+			{
+				inversion_lignes(m, i, l_pivot);
+				inversion_lignes(inverse, i, l_pivot);
+			}
+printf("l_pivot : %d :: c_pivot : %d\n",l_pivot,c_pivot);
+printf(" m avant : \n");
+display_matrix(m);
+printf(" inverse avant : \n");
+display_matrix(inverse);
+			for(j = 0 ; j < m->nbl ; j++)
+			{
+				if(j != i && m->matrice[j][c_pivot] != 0)
+				{
+					coeff = - ( m->matrice[j][c_pivot] / m->matrice[i][c_pivot] );
+					addition_lignes(m, i, j, coeff);
+					addition_lignes(inverse, i, j, coeff);
+				}
+			}
+printf(" après  m : \n");
+display_matrix(m);
+printf(" après inverse : \n");
+display_matrix(inverse);
+printf("\033[31m=========================================================\033[00m\n");
+			c_pivot++;
+		}
+	}
 
 	return inverse;
 }
@@ -35,4 +71,19 @@ matrice_s * inversion_comatrices(matrice_s * m)
 			tmp->matrice[i][j] /= det;
 
 	return tmp;
+}
+
+// Recherche la ligne du prochain pivot, en partant d'une ligne et d'une colonne
+// Retourne la ligne pivot ou -1 s'il n'y a pas 
+int recherche_ligne_pivot_suivant(matrice_s * m, int l_depart, int * c_depart)
+{
+	int indice = -1,i, *j;
+
+	for( j = c_depart ; indice == -1 && *j < m->nbc ; j[0]++)
+		for(i = l_depart ; indice == -1 && i < m->nbl; i++)
+			if(m->matrice[i][*j] != 0)
+				indice = i;
+
+	j[0]--;
+	return indice;
 }
--- licence_s4/algebre/inversion.h
+++ licence_s4/algebre/inversion.h
@@ -4,4 +4,9 @@
 matrice_s * inversion_gauss_jordan(matrice_s * m);
 matrice_s * inversion_comatrices(matrice_s * m);
 
+// Recherche la ligne du prochain pivot, en partant d'une ligne et d'une colonne
+// Retourne la ligne pivot ou -1 s'il n'y a pas 
+// Et la colonne pivot est renseignée
+int recherche_ligne_pivot_suivant(matrice_s * m, int l_depart, int * c_depart);
+
 #endif
