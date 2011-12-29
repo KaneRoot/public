14bfc67a3768f89bb588939a7b6e39ed6a58ff2b
Philippe Pittoli <kane.root@gmail.com>
2011-12-29 15:45:17 +0100

    correction de l_pivot qui doit changer après l'échange de lignes dans la matrice

--- licence_s4/algebre/pivot-gauss.c
+++ licence_s4/algebre/pivot-gauss.c
@@ -10,8 +10,8 @@ int pivot_gauss_double_matrice(matrice_s * m1, matrice_s * m2)
 {
 	int i, j, l_pivot = 0, c_pivot = 0, retour = 0;
 	float coeff;
-	matrice_s * m;
-	m = dupliquer_matrice(m1);
+	matrice_s * m, *identitee;
+	m = dupliquer_matrice(m1); // Pour ne pas modifier directement la matrice m1
 
 	for(i = 0 ; i < m->nbl ; i++)
 	{
@@ -23,6 +23,7 @@ int pivot_gauss_double_matrice(matrice_s * m1, matrice_s * m2)
 				inversion_lignes(m2, i, l_pivot);
 			}
 
+			l_pivot = i;
 			// multiplication ligne pivot par 1/pivot
 			coeff = 1/m->matrice[l_pivot][c_pivot];
 			for(j = 0 ; j < m->nbc ; j++)
@@ -30,39 +31,30 @@ int pivot_gauss_double_matrice(matrice_s * m1, matrice_s * m2)
 			for(j = 0 ; j < m2->nbc ; j++)
 				m2->matrice[l_pivot][j] *= coeff;
 
-//	printf(" avant  m : \n");
-//	display_matrix(m);
-//	printf(" avant m2 : \n");
-//	display_matrix(m2);
-//	printf("\033[31m=========================================\033[00m\n");
-
 			for(j = 0 ; j < m->nbl ; j++)
 			{
-				printf("Je suis arrivé jusque-là 2!\n");
 				if(j != i && m->matrice[j][c_pivot] != 0)
 				{
 					coeff = - ( m->matrice[j][c_pivot] / m->matrice[i][c_pivot] );
 					addition_lignes(m, i, j, coeff);
 					addition_lignes(m2, i, j, coeff);
 				}
-				printf("Je suis arrivé jusque-là 3!\n");
 			}
-//	printf(" après  m : \n");
-//	display_matrix(m);
-//	printf(" après m2 : \n");
-//	display_matrix(m2);
-//	printf("\033[31m=========================================\033[00m\n");
 			c_pivot++;
 		}
 	}
 
-	matrice_s * identitee = matrice_identitee(m->nbl);
+	// On vérifie qu'une fois qu'on a terminé m1 = matrice identité
+	// Sinon : erreur dans le pivot
+	identitee = matrice_identitee(m->nbl);
 	if(identiques(identitee, m) == 0)
 		retour = 0;
 	else
 		retour = 1;
 
+	free_matrix(m);
 	free_matrix(identitee);
+
 	return retour;
 }
 
