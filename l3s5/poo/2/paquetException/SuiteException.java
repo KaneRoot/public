2c1b6c7a00ef24481970b12b80df8e319980cbac
Philippe Pittoli <kane.root@gmail.com>
2012-02-20 12:47:42 +0100

    ajout de la doc java pour l'exception

--- l3s5/poo/2/paquetException/SuiteException.java
+++ l3s5/poo/2/paquetException/SuiteException.java
@@ -1,12 +1,32 @@
+/**
+ * @author Karchnu
+ * @version 1.0-alpha
+ * @since 2012-02-20
+ */
+
 package paquetException;
 
 public class SuiteException extends Exception
 {
+	/**
+	 * erreur : chaîne de caractères contenant l'erreur
+	 */
 	String erreur;
+
+	/**
+	 * Constructeur de l'exception
+	 *
+	 * @param nom : nom de l'erreur
+	 */
 	public SuiteException(String nom)
 	{
 		this.erreur = nom;
 	}
+	/**
+	 * retourne le message d'erreur
+	 *
+	 * @return String_erreur
+	 */
 	public String getMessage()
 	{
 		return this.erreur;
