2812b855f53d5067bd98313590a153dfb6398051
Philippe Pittoli <kane.root@gmail.com>
2012-03-13 13:00:44 +0100

    Presque propre

--- ls5/bdd/projet/creation.sql
+++ ls5/bdd/projet/creation.sql
@@ -24,21 +24,27 @@ create table VOL
 	dateArrivee date,
 	villeDepart varchar2(50),
 	villeArrivee varchar2(50),
-	idCompagnie number(10) NOT NULL,
+	idCompagnie number(10) PRIMARY KEY,
 	CONSTRAINT fk_vol_compagnie FOREIGN KEY (idCompagnie) REFERENCES COMPAGNIE ON DELETE CASCADE
 );
 create table BILLET
 (
 	idBillet number(15) PRIMARY KEY,
-	promo number(2),
+	promo number(2) CHECK (promo > 0),
+	idVol number(10) PRIMARY KEY,
+	idCompagnie number(10) PRIMARY KEY,
+	CONSTRAINT fk_billet_vol FOREIGN KEY (idVol) REFERENCES VOL ON DELETE CASCADE,
+	CONSTRAINT fk_billet_compagnie FOREIGN KEY (idCompagnie) REFERENCES COMPAGNIE ON DELETE CASCADE
+);
+create table ACHAT
+(
+	idAchat number(15) PRIMARY KEY,
+	idBillet number(15) NOT NULL,
+	idClient number(15) NOT NULL,
 	prix number(15) CHECK (prix > 0),
 	dateAchat date,
-	idVol number(10) NOT NULL,
-	idClient number(15) NOT NULL,
-	idCompagnie number(10) NOT NULL,
-	CONSTRAINT fk_billet_vol FOREIGN KEY (idVol) REFERENCES VOL ON DELETE CASCADE,
-	CONSTRAINT fk_billet_client FOREIGN KEY (idClient) REFERENCES CLIENT ON DELETE CASCADE ,
-	CONSTRAINT fk_billet_compagnie FOREIGN KEY (idCompagnie) REFERENCES COMPAGNIE ON DELETE CASCADE
+	CONSTRAINT fk_achat_client FOREIGN KEY (idClient) REFERENCES CLIENT ON DELETE CASCADE ,
+	CONSTRAINT fk_achat_billet FOREIGN KEY (idBillet) REFERENCES BILLET ON DELETE CASCADE
 );
 create table CARTE_FIDELITE
 (
@@ -52,7 +58,7 @@ create table RESERVATION
 (
 	idReservation number(15) PRIMARY KEY,
 	dateReservation date,
-	etatReservation char(1),
+	etatReservation char(1) CHECK (etatReservation = 'R' OR etatReservation = 'A'),
 	idClient number(15) NOT NULL,
 	idBillet number(15) NOT NULL,
 	CONSTRAINT fk_reservation_client FOREIGN KEY (idClient) REFERENCES CLIENT ON DELETE CASCADE,
