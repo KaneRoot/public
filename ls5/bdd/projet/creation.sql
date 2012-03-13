da20460343e9dd91abc75f2ab52c2313f46ffdf7
Philippe Pittoli <kane.root@gmail.com>
2012-03-13 13:23:15 +0100

    dernière version en date

--- ls5/bdd/projet/creation.sql
+++ ls5/bdd/projet/creation.sql
@@ -1,38 +1,38 @@
 create table CLIENT
 (
 	idClient number(15) PRIMARY KEY,
-	nomClient varchar2(50),
-	prenomClient varchar2(50),
-	adresseClient varchar2(100),
+	nomClient varchar2(50) NOT NULL,
+	prenomClient varchar2(50) NOT NULL,
+	adresseClient varchar2(100) NOT NULL,
 	numTelClient number(10)
 );
 create table COMPAGNIE
 (
 	idCompagnie number(10) PRIMARY KEY,
-	nomCompagnie varchar2(50)
+	nomCompagnie varchar2(50) NOT NULL
 );
 create table ESCALE
 (
 	idEscale number(10) PRIMARY KEY,
-	villeDepart varchar2(50),
-	villeArrivee varchar2(50)	
+	villeDepart varchar2(50) NOT NULL,
+	villeArrivee varchar2(50) NOT NULL	
 );
 create table VOL
 (
 	idVol number(10) PRIMARY KEY,
-	dateDepart date,
-	dateArrivee date,
-	villeDepart varchar2(50),
-	villeArrivee varchar2(50),
 	idCompagnie number(10) PRIMARY KEY,
+	dateDepart date NOT NULL,
+	dateArrivee date NOT NULL,
+	villeDepart varchar2(50) NOT NULL,
+	villeArrivee varchar2(50) NOT NULL,
 	CONSTRAINT fk_vol_compagnie FOREIGN KEY (idCompagnie) REFERENCES COMPAGNIE ON DELETE CASCADE
 );
 create table BILLET
 (
 	idBillet number(15) PRIMARY KEY,
-	promo number(2) CHECK (promo > 0),
 	idVol number(10) PRIMARY KEY,
 	idCompagnie number(10) PRIMARY KEY,
+	promo number(2) CHECK (promo > 0 AND promo < 100),
 	CONSTRAINT fk_billet_vol FOREIGN KEY (idVol) REFERENCES VOL ON DELETE CASCADE,
 	CONSTRAINT fk_billet_compagnie FOREIGN KEY (idCompagnie) REFERENCES COMPAGNIE ON DELETE CASCADE
 );
@@ -42,22 +42,22 @@ create table ACHAT
 	idBillet number(15) NOT NULL,
 	idClient number(15) NOT NULL,
 	prix number(15) CHECK (prix > 0),
-	dateAchat date,
+	dateAchat date NOT NULL,
 	CONSTRAINT fk_achat_client FOREIGN KEY (idClient) REFERENCES CLIENT ON DELETE CASCADE ,
 	CONSTRAINT fk_achat_billet FOREIGN KEY (idBillet) REFERENCES BILLET ON DELETE CASCADE
 );
 create table CARTE_FIDELITE
 (
-	idCompagnie number(8) NOT NULL, 
-	idClient number(6) NOT NULL,
-	miles number(10),
+	idCompagnie number(8) PRIMARY KEY, 
+	idClient number(6) PRIMARY KEY,
+	miles number(10) NOT NULL,
 	CONSTRAINT fk_carte_fidelite_compagnie FOREIGN KEY (idCompagnie) REFERENCES COMPAGNIE ON DELETE CASCADE,
 	CONSTRAINT fk_carte_fidelite_client FOREIGN KEY (idClient) REFERENCES CLIENT ON DELETE CASCADE
 );
 create table RESERVATION
 (
 	idReservation number(15) PRIMARY KEY,
-	dateReservation date,
+	dateReservation date NOT NULL,
 	etatReservation char(1) CHECK (etatReservation = 'R' OR etatReservation = 'A'),
 	idClient number(15) NOT NULL,
 	idBillet number(15) NOT NULL,
