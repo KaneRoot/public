fc3c5af6e7dc8967a68da914529ee6e08d65d6e7
Philippe Pittoli <kane.root@gmail.com>
2012-03-13 11:35:33 +0100

    TP2 BDD

--- ls5/bdd/projet.sql
+++ ls5/bdd/projet.sql
@@ -1,62 +1,67 @@
 create table CLIENT
-{
-	id_client number(15) PRIMARY KEY,
-	nom_client varchar2(50),
-	prenom_client varchar2(50),
-	adresse_client varchar2(100),
-	num_tel_client number(10)
-};
+(
+	idClient number(15) PRIMARY KEY,
+	nomClient varchar2(50),
+	prenomClient varchar2(50),
+	adresseClient varchar2(100),
+	numTelClient number(10)
+);
 create table VOL
-{
-	id_vol number(10) PRIMARY KEY,
-	date_depart date,
-	date_arrivee date,
-	ville_depart varchar2(50),
-	ville_arrivee varchar2(50),
-	CONSTRAINT fk_vol_compagnie FOREIGN KEY(id_compagnie) REFERENCES COMPAGNIE
-};
+(
+	idVol number(10) PRIMARY KEY,
+	dateDepart date,
+	dateArrivee date,
+	villeDepart varchar2(50),
+	villeArrivee varchar2(50),
+	CONSTRAINT fk_vol_compagnie FOREIGN KEY(idCompagnie) REFERENCES COMPAGNIE
+);
 create table ESCALE
-{
-	id_escale number(10) PRIMARY KEY,
-	ville_depart varchar2(50),
-	ville_arrivee varchar2(50)	
-};
+(
+	idEscale number(10) PRIMARY KEY,
+	villeDepart varchar2(50),
+	villeArrivee varchar2(50)	
+);
 create table COMPAGNIE
-{
-	id_compagnie number(10) PRIMARY KEY,
-	nom_compagnie varchar2(50)
-};
+(
+	idCompagnie number(10) PRIMARY KEY,
+	nomCompagnie varchar2(50)
+);
 create table CARTE_FIDELITE
-{
-	id_compagnie number(8) NOT NULL, 
-	id_client number(6) NOT NULL,
+(
+	idCompagnie number(8) NOT NULL, 
+	idClient number(6) NOT NULL,
 	miles number(10),
-	CONSTRAINT fk_carte_fidelite_compagnie FOREIGN KEY (id_compagnie) REFERENCES COMPAGNIE,
-	CONSTRAINT fk_carte_fidelite_client FOREIGN KEY (id_client) REFERENCES CLIENT
-};
+	CONSTRAINT fk_carte_fidelite_compagnie FOREIGN KEY (idCompagnie) REFERENCES COMPAGNIE ON DELETE CASCADE,
+	CONSTRAINT fk_carte_fidelite_client FOREIGN KEY (idClient) REFERENCES CLIENT ON DELETE CASCADE
+);
 create table RESERVATION
-{
-	id_reservation number(15) PRIMARY KEY,
-	date_reservation date,
-	etat_reservation char(1),
-	id_client number(15) NOT NULL,
-	id_billet number(15) NOT NULL,
-	CONSTRAINT fk_reservation_client FOREIGN KEY (id_client) REFERENCES CLIENT,
-	CONSTRAINT fk_reservation_billet FOREIGN KEY (id_billet) REFERENCES BILLET
-};
+(
+	idReservation number(15) PRIMARY KEY,
+	dateReservation date,
+	etatReservation char(1),
+	idClient number(15) NOT NULL,
+	idBillet number(15) NOT NULL,
+	CONSTRAINT fk_reservation_client FOREIGN KEY (idClient) REFERENCES CLIENT ON DELETE CASCADE,
+	CONSTRAINT fk_reservation_billet FOREIGN KEY (idBillet) REFERENCES BILLET ON DELETE CASCADE
+);
 create table BILLET
-{
-	id_billet number(15) PRIMARY KEY,
+(
+	idBillet number(15) PRIMARY KEY,
 	promo number(2),
-	prix number(15),
-	date_achat date,
+	prix number(15) CHECK (prix > 0),
+	dateAchat date,
 
-	id_vol number(10) NOT NULL,
-	id_client number(15) NOT NULL,
-	id_compagnie number(10) NOT NULL,
-	CONSTRAINT fk_billet_vol FOREIGN KEY (id_vol) REFERENCES VOL,
-	CONSTRAINT fk_billet_client FOREIGN KEY (id_client) REFERENCES CLIENT,
-	CONSTRAINT fk_billet_compagnie FOREIGN KEY (id_compagnie) REFERENCES COMPAGNIE
-};
-
-
+	idVol number(10) NOT NULL,
+	idClient number(15) NOT NULL,
+	idCompagnie number(10) NOT NULL,
+	CONSTRAINT fk_billet_vol FOREIGN KEY (idVol) REFERENCES VOL ON DELETE CASCADE,
+	CONSTRAINT fk_billet_client FOREIGN KEY (idClient) REFERENCES CLIENT ON DELETE CASCADE ,
+	CONSTRAINT fk_billet_compagnie FOREIGN KEY (idCompagnie) REFERENCES COMPAGNIE ON DELETE CASCADE
+);
+create table VOL_ESCALE
+(
+	idVol number(10) NOT NULL,
+	idEscale number(10) NOT NULL,
+	CONSTRAINT fk_vol_escale_vol FOREIGN KEY (idVol) REFERENCES VOL ON DELETE CASCADE,
+	CONSTRAINT fk_vol_escale_escale FOREIGN KEY (idEscale) REFERENCES ESCALE ON DELETE CASCADE
+);
--- ls5/bdd/projet_suppression.sql
+++ ls5/bdd/projet_suppression.sql
@@ -6,5 +6,14 @@ alter table RESERVATION drop CONSTRAINT fk_reservation_billet;
 alter table BILLET drop CONSTRAINT fk_billet_vol;
 alter table BILLET drop CONSTRAINT fk_billet_client;
 alter table BILLET drop CONSTRAINT fk_billet_compagnie;
+alter table VOL_ESCALE drop CONSTRAINT fk_vol_escale_vol;
+alter table VOL_ESCALE drop CONSTRAINT fk_vol_escale_escale;
 
-
+drop table CLIENT;
+drop table VOL;
+drop table ESCALE;
+drop table COMPAGNIE;
+drop table CARTE_FIDELITE; 
+drop table RESERVATION;
+drop table BILLET;
+drop table VOL_ESCALE;
