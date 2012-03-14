772121004f5705ea874f42692063ebf28a06deff
Philippe Pittoli <kane.root@gmail.com>
2012-03-14 15:19:05 +0100

    dernière version avant de partir chez mua

--- ls5/reseau/3/code_correcteur_bit_parite/dotest.sh
+++ ls5/reseau/3/code_correcteur_bit_parite/dotest.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-./medium 1 2>/dev/null 1>&2 &
-./receiver FileReceived 2>/dev/null 1>&2 &
+./medium 1 &
+./receiver FileReceived &
 ./sender FileToSend
-
-killall receiver & 2>/dev/null
-killall medium & 2>/dev/null
-killall sender & 2>/dev/null
+#
+#killall receiver & 2>/dev/null
+#killall medium & 2>/dev/null
+#killall sender & 2>/dev/null
