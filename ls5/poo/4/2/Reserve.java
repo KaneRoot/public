4c94a4cffc31dfc8eeb59091ec996f943c63ef79
karchnu <kane.root@gmail.com>
2012-03-26 12:53:02 +0200

    POO - TP plus comdensé

--- ls5/poo/4/2/Reserve.java
+++ ls5/poo/4/2/Reserve.java
@@ -5,48 +5,27 @@ public class Reserve
 	{
 		while((nbressources - nb) < 0)
 		{
-			try
-			{
-				wait();
-			}
-			catch(InterruptedException e)
-			{
-				System.out.println("Exception prendre : " + e.getMessage());
-			}
+			try { wait(); }
+			catch(InterruptedException e) { System.out.println("Exception prendre : " + e.getMessage()); }
 		}
 		nbressources -= nb;
 		System.out.println("Prendre " + nb + " résultat : " + nbressources);
-		try
-		{
-			notifyAll();
-		}
-		catch(IllegalMonitorStateException e)
-		{
-			System.out.println("Exception notify : " + e.getMessage());
-		}
+
+		try { notifyAll(); }
+		catch(IllegalMonitorStateException e) { System.out.println("Exception notify : " + e.getMessage()); }
 	}
 	public synchronized void mettre(int nb)
 	{
 		while((nbressources + nb) > nbmax)
 		{
-			try
-			{
-				wait();
-			}
-			catch(InterruptedException e)
-			{
-				System.out.println("Exception mettre : " + e.getMessage());
-			}
+			try { wait(); } 
+			catch(InterruptedException e) { System.out.println("Exception mettre : " + e.getMessage()); }
 		}
+
 		nbressources += nb;
 		System.out.println("Mettre " + nb + " résultat : " + nbressources);
-		try
-		{
-			notifyAll();
-		}
-		catch(IllegalMonitorStateException e)
-		{
-			System.out.println("Exception notify : " + e.getMessage());
-		}
+
+		try { notifyAll(); } 
+		catch(IllegalMonitorStateException e) { System.out.println("Exception notify : " + e.getMessage()); }
 	}
 }
