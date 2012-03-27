92d85420572a8ed17719bc5af134a85430a6525c
Philippe Pittoli <kane.root@gmail.com>
2012-03-27 11:09:14 +0200

    Quelques insertions de plus et les procédures nbbillets achetés et réservés fonctionnent

--- ls5/bdd/projet/plsql/nbbillets.sql
+++ ls5/bdd/projet/plsql/nbbillets.sql
@@ -1,6 +1,6 @@
--- Billets réservés
-CREATE or REPLACE function nb_billet_reserve(	IN idVol_p VOL.idVol%TYPE,  
-												IN idCompagnie_p COMPAGNIE.idCompagnie%TYPE)
+---- Billets réservés
+CREATE or REPLACE function nb_billet_reserve(	idVol_p VOL.idVol%TYPE,  
+												idCompagnie_p COMPAGNIE.idCompagnie%TYPE)
 	RETURN INTEGER
 	IS
 		nb_billet_reserve_v INTEGER;
@@ -10,11 +10,14 @@ CREATE or REPLACE function nb_billet_reserve(	IN idVol_p VOL.idVol%TYPE,
 			FROM BILLET 
 			WHERE idVol = idVol_p AND idCompagnie = idCompagnie_p AND etatBillet = 'R';
 		RETURN nb_billet_reserve_v;
-END ;
+END;
+/
+SHOW ERRORS FUNCTION nb_billet_reserve;
 
--- Billets achetés
-CREATE or REPLACE function nb_billet_achetes(	IN idVol_p VOL.idVol%TYPE,  
-												IN idCompagnie_p COMPAGNIE.idCompagnie%TYPE)
+---- procedure 
+---- Billets achetés
+CREATE or REPLACE function nb_billet_achetes(	idVol_p VOL.idVol%TYPE,  
+												idCompagnie_p COMPAGNIE.idCompagnie%TYPE)
 	RETURN INTEGER
 	IS
 		nb_billet_achete_v INTEGER;
@@ -25,3 +28,9 @@ CREATE or REPLACE function nb_billet_achetes(	IN idVol_p VOL.idVol%TYPE,
 			WHERE idVol = idVol_p AND idCompagnie = idCompagnie_p AND etatBillet = 'A';
 		RETURN nb_billet_achete_v;
 END ;
+/
+SHOW ERRORS FUNCTION nb_billet_achetes;
+
+-- tests
+select nb_billet_reserve(1,1) from dual;
+select nb_billet_achetes(1,1) from dual;
