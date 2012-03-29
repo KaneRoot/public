f4e5a5529c17afe60ecc3c9127e44156a51c1b54
Philippe Pittoli <kane.root@gmail.com>
2012-03-29 23:10:15 +0200

    quelques modifications

--- ls5/bdd/projet/insertion.sql
+++ ls5/bdd/projet/insertion.sql
@@ -20,9 +20,10 @@ INSERT INTO VILLE VALUES(2, 'LARIS');
 INSERT INTO VILLE VALUES(3, 'SARIS');
 
 -- idVol idCompagnie idVilleDepart idVilleArrivee dateDepart dateArrivee
-INSERT INTO VOL VALUES(1, 1, 1, 3, to_date('2012/03/16 21:02:44', 'yyyy/mm/dd hh24:mi:ss'), to_date('2012/03/18 21:02:44', 'yyyy/mm/dd hh24:mi:ss'));
-INSERT INTO VOL VALUES(2, 1, 2, 3, to_date('2012/03/16 21:02:44', 'yyyy/mm/dd hh24:mi:ss'), to_date('2012/03/18 21:02:44', 'yyyy/mm/dd hh24:mi:ss'));
-INSERT INTO VOL VALUES(3, 1, 3, 1, to_date('2012/03/16 21:02:44', 'yyyy/mm/dd hh24:mi:ss'), to_date('2012/03/18 21:02:44', 'yyyy/mm/dd hh24:mi:ss'));
+INSERT INTO VOL VALUES(1, 1, 1, 3, to_date('2012/03/16 21:02:44', 'yyyy/mm/dd hh24:mi:ss'), to_date('2012/04/18 21:02:44', 'yyyy/mm/dd hh24:mi:ss'));
+INSERT INTO VOL VALUES(1, 2, 1, 3, to_date('2012/03/29 18:02:44', 'yyyy/mm/dd hh24:mi:ss'), to_date('2012/04/18 21:02:44', 'yyyy/mm/dd hh24:mi:ss'));
+INSERT INTO VOL VALUES(2, 1, 2, 3, to_date('2012/03/16 21:02:44', 'yyyy/mm/dd hh24:mi:ss'), to_date('2012/04/18 21:02:44', 'yyyy/mm/dd hh24:mi:ss'));
+INSERT INTO VOL VALUES(3, 1, 3, 1, to_date('2012/03/16 21:02:44', 'yyyy/mm/dd hh24:mi:ss'), to_date('2012/04/18 21:02:44', 'yyyy/mm/dd hh24:mi:ss'));
 
 -- idEscale idVille idVol idCompagnie
 INSERT INTO ESCALE VALUES(1, 1, 1, 1);
@@ -33,7 +34,7 @@ INSERT INTO ESCALE VALUES(3, 3, 1, 1);
 INSERT INTO CARTE_FIDELITE VALUES(1, 1, 900);
 INSERT INTO CARTE_FIDELITE VALUES(2, 1, 950);
 
--- BILLET idBillet idVol, idCompagnie, prix, promo, etatBillet
+-- idBillet idVol idCompagnie prix promo etatBillet
 INSERT INTO BILLET VALUES(1, 1, 1, 1000, 0, null);
 INSERT INTO BILLET VALUES(2, 1, 1, 1000, 0, null);
 INSERT INTO BILLET VALUES(3, 1, 1, 1000, 0, null);
@@ -43,8 +44,9 @@ INSERT INTO BILLET VALUES(6, 1, 1, 1000, 0, 'A');
 INSERT INTO BILLET VALUES(7, 1, 1, 1000, 0, 'R');
 INSERT INTO BILLET VALUES(8, 1, 1, 1000, 0, 'R');
 
--- BILLET_CLIENT idAchat, idBillet, idClient, dateAchat
+-- idAchat idBillet idClient dateAchat
 INSERT INTO BILLET_CLIENT VALUES(1, 1, 1, DEFAULT);
+INSERT INTO BILLET_CLIENT VALUES(2, 2, 1, DEFAULT);
 
 -- erreur volontaire
 -- INSERT INTO CLIENT VALUES(3, 'moi3', 'prenomdemoi3', 'adresse3', 1050305010); 
--- ls5/bdd/projet/plsql/etatreservation.sql
+++ ls5/bdd/projet/plsql/etatreservation.sql
@@ -1,7 +1,9 @@
 -- b. Alerter un client une heure avant l'expiration de sa réservation.
+set serveroutput on;
+
 CREATE or REPLACE function etat_reservation(	idBillet_p RESERVATION.idBillet%TYPE,
 												idClient_p RESERVATION.idClient%TYPE)
-	RETURN BOOLEAN
+	RETURN NUMBER
 	IS
 		date_vol_v DATE;
 		date_reservation_v DATE;
@@ -9,33 +11,39 @@ CREATE or REPLACE function etat_reservation(	idBillet_p RESERVATION.idBillet%TYP
 		idCompagnie_v COMPAGNIE.idCompagnie%TYPE;
 
 	BEGIN 
+		dbms_output.enable(1000);
 		-- récupération des infos sur le vol (idVol + idCompagnie)
 		SELECT idVol, idCompagnie INTO idVol_v, idCompagnie_v
 			FROM BILLET
 			WHERE idBillet = idBillet_p;
 
+--		dbms_output.put_line('idVol ' || idVol_v || ' idCompagnie_p ' || idCompagnie_v);
+--		dbms_output.put_line('idClient ' || idClient_p || ' idBillet_p ' || idBillet_p);
+
 		-- récupération de la date de départ
-		SELECT dateDepart INTO date_vol_v
+		SELECT v.dateDepart INTO date_vol_v
 			FROM VOL v
-			WHERE v.dateDepart = date_vol_v and v.idVol = idVol_v and v.idCompagnie = idCompagnie_v;
+			WHERE v.idVol = idVol_v and v.idCompagnie = idCompagnie_v;
 
 		-- récupération de la date de réservation
-		SELECT r.dateReservation INTO date_reservation_v
-			FROM RESERVATION r
-			WHERE idBillet_p = r.idBillet and idClient_p = r.idClient ;
+		SELECT bc.dateAchat INTO date_reservation_v 
+			FROM BILLET_CLIENT bc
+			WHERE bc.idBillet = idBillet_p and bc.idClient = idClient_p ;
 
 		-- si date actuelle + 3 jours + 1h >= décollage de l'avion : alerte
 		-- si date de réservation + 2 jours + 1h <= date actuelle : alerte
 		IF (SYSDATE + 1/24 + 3) >= date_vol_v OR (date_reservation_v + 2 + 1/24) <= SYSDATE 
 		THEN 
-			RETURN TRUE;
+			RETURN 1;
 		END IF;
 
 		-- sinon pas d'alerte
-		RETURN FALSE;
+		RETURN 0;
 END ;
 /
 SHOW ERRORS FUNCTION etat_reservation;
 
 -- test
-select etat_reservation(
+-- idBillet idClient
+select etat_reservation(3,1) from dual;
+select etat_reservation(1,1) from dual;
