1cd132175465b36892749083deca3b20d693f158
Philippe Pittoli <kane.root@gmail.com>
2012-04-02 11:49:08 +0200

    quelques maj de procédure SQL

--- ls5/bdd/projet/plsql/majmiles.sql
+++ ls5/bdd/projet/plsql/majmiles.sql
@@ -1,41 +1,30 @@
 -- a. Mettre à jour le compteur des miles d'un client donné en prenant en compte l'option de réduction de 100€ pour l'achat du prochain billet(i.e nb_miles = 1000).
-CREATE or REPLACE function maj_miles(	idClient_p CLIENT.id_client%TYPE, 
+CREATE or REPLACE procedure maj_miles(	idClient_p CLIENT.idClient%TYPE, 
 										idCompagnie_p COMPAGNIE.idCompagnie%TYPE)
-	RETURN Boolean  -- renvoie vrai si on baisse le prix du billet
 	IS
-		miles_v carte_fidelie.miles%TYPE;
+		miles_v CARTE_FIDELITE.miles%TYPE;
+
 	BEGIN 
 		SELECT miles INTO miles_v
 			FROM CARTE_FIDELITE c
 			WHERE c.idClient = idClient_p AND c.idCompagnie = idCompagnie_p;
 
-		IF miles_v >= 1000 THEN    
-			UPDATE carte_fidelite 
-				set miles = miles_v - 900
+		IF miles_v >= 1000 
+		THEN    
+			miles_v := miles_v - 900;
+			UPDATE CARTE_FIDELITE
+				set miles = miles_v
 				WHERE idClient = idClient_p and idCompagnie = idCompagnie_p;
-			return TRUE ;
 		END IF;
 
+		miles_v := miles_v + 100;
 		UPDATE CARTE_FIDELITE 
-			set miles = miles_v + 100
+			set miles = miles_v
 			WHERE idClient = idClient_p and idCompagnie = idCompagnie_p;
-		return FALSE;
 END;
 /
-SHOW ERRORS FUNCTION maj_miles;
+SHOW ERRORS procedure maj_miles;
+
 
 -- test
-select maj_miles(1, 1) from dual;
-select maj_miles(1, 1) from dual;
-select maj_miles(1, 1) from dual;
-select maj_miles(1, 1) from dual;
-select maj_miles(1, 1) from dual;
-select maj_miles(1, 1) from dual;
-select maj_miles(1, 1) from dual;
-select maj_miles(1, 1) from dual;
-select maj_miles(1, 1) from dual;
-select maj_miles(1, 1) from dual;
-select maj_miles(1, 1) from dual;
-select maj_miles(1, 1) from dual;
-select maj_miles(1, 1) from dual;
-select maj_miles(1, 1) from dual;
+execute maj_miles(1,1);
