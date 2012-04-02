a16e36573d33a38233dba8e58c2830e0fcb9ad86
Philippe Pittoli <kane.root@gmail.com>
2012-04-02 23:56:31 +0200

    achat + réservation

---
+++ ls5/bdd/projet/plsql/achat.sql
@@ -0,0 +1,48 @@
+create or REPLACE procedure achat(	idClient_p CLIENT.idClient%TYPE,
+									idBillet_p BILLET.idBillet%TYPE)
+IS
+	idCompagnie_v COMPAGNIE.idCompagnie%TYPE;
+	miles_v CARTE_FIDELITE.miles%TYPE;
+	miles2_v CARTE_FIDELITE.miles%TYPE;
+BEGIN
+	-- on récupère l'idCompagnie
+	select idCompagnie into idCompagnie_v
+		from BILLET
+		where idBillet = idBillet_p;
+
+	-- s'il doit avoir une réduction de prix
+	select miles into miles_v
+		from CARTE_FIDELITE
+		where idClient = idClient_p and idCompagnie = idCompagnie_v;
+	
+	-- on met à jour les miles
+	execute immediate majMiles(idClient_p,idCompagnie_v);
+
+	-- nouveau nb de miles
+	select miles into miles2_v
+		from CARTE_FIDELITE
+		where idClient = idClient_p and idCompagnie = idCompagnie_v;
+	
+	-- si nb miles actuels < ancien = 100€ de réduction
+	if miles_v > miles2_v
+	then
+		UPDATE BILLET SET prix=prix-100 where idBillet = idBillet_p;
+	end if;
+
+	-- si on a une réservation	
+	UPDATE BILLET SET etatBillet='A' WHERE idBillet = idBillet_p;
+
+END;
+/
+SHOW ERRORS procedure achat;
+
+create or REPLACE procedure reserver(	idClient_p CLIENT.idClient%TYPE,
+											idBillet_p BILLET.idBillet%TYPE)
+IS
+
+BEGIN
+	INSERT INTO RESERVATION VALUES(SYSDATE, idClient_p, idBillet_p);
+	UPDATE BILLET SET etatBillet='R' WHERE idBillet = idBillet_p;
+END;
+/
+SHOW ERRORS procedure reserver;
