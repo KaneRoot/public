3b4c2692f17c0a3e380bcc752c6d7d643919c7bf
karchnu <kane.root@gmail.com>
2012-04-05 14:35:23 +0200

    quelques changements

--- ls5/bdd/projet/site/connexion.php
+++ ls5/bdd/projet/site/connexion.php
@@ -2,6 +2,20 @@
 	session_start();
 	$_TITRE_PAGE="BDD - connexion client";
 	include("includes/in_entete"); 
+
+
+	//$conn = oci_connect("log", "pass", "host:dbname");
+	$conn = oci_connect("pittoli", "1712juph", "codd:PITTOLI");
+
+	$query = "SELECT * from info_connect";
+	if(! $conn)
+	{
+		echo "Erreur de connexion";
+		exit;
+	}
+	$stmt = oci_parse($conn, $query);
+	oci_execute($stmt);
+
 ?>
 	<!-- container -->
 	<div class="container">
@@ -9,4 +23,6 @@
 		<?php include("includes/in_haut"); ?>
 
 	</div>
-<?php include("includes/in_pied"); ?>
+<?php 
+	include("includes/in_pied"); 
+?>
