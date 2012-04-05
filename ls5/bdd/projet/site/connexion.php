69f2a42fa4d0b7bb94c6de0db441d6388b649472
Philippe Pittoli <kane.root@gmail.com>
2012-04-05 15:22:15 +0200

    voilà ça marche, go

--- ls5/bdd/projet/site/connexion.php
+++ ls5/bdd/projet/site/connexion.php
@@ -2,19 +2,28 @@
 	session_start();
 	$_TITRE_PAGE="BDD - connexion client";
 	include("includes/in_entete"); 
-
-
 	//$conn = oci_connect("log", "pass", "host:dbname");
-	$conn = oci_connect("pittoli", "1712juph", "codd:PITTOLI");
-
-	$query = "SELECT * from info_connect";
+	$conn = oci_connect("pittoli", "1712juph", "localhost/ROSA");
 	if(! $conn)
 	{
 		echo "Erreur de connexion";
 		exit;
 	}
+
+	$query = "select * from CLIENT";
 	$stmt = oci_parse($conn, $query);
 	oci_execute($stmt);
+
+	echo "vous êtes : ". $_SERVER["REMOTE_ADDR"] . "\n<br/>";
+	while($array = oci_fetch_assoc($stmt))
+	{
+		echo "<li><ul>";
+
+		foreach(array_keys($array) as $key)
+			echo "<li>" . $key . " " . $array[$key]. "</li>\n";
+
+		echo "</ul></li>";
+	}
 
 ?>
 	<!-- container -->
