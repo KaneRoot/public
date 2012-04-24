a605c9c9f974e30d4847c8cad0101ea2ed3a0f0b
Philippe Pittoli <kane.root@gmail.com>
2012-04-24 14:16:20 +0200

    Je peux afficher des vols de réservation, mais pas encore réserver

--- ls5/bdd/projet/site/vols.php
+++ ls5/bdd/projet/site/vols.php
@@ -3,9 +3,25 @@
 session_start();
 
 include("co.php");
+include("fonctions.php");
 
-$_TITRE_PAGE="BDD - vols";
+$_TITRE_PAGE="BDD - vols - réservation";
 include("includes/in_entete"); 
+
+if( isset($_GET['restart']))
+{
+	unset($_SESSION['ville_depart'], $_SESSION['ville_arrivee'], $_SESSION['vol']);
+}
+
+if( isset($_POST['ville_depart']) )
+{
+	$_SESSION['ville_depart'] = $_POST['ville_depart'];
+}
+if( isset($_SESSION['ville_depart']))
+	echo "Ville de départ : " . $_SESSION['ville_depart'];
+
+if( isset($_POST['ville_arrivee']) )
+	$_SESSION['ville_arrivee'] = $_POST['ville_arrivee'];
 
 ?>
 	<!-- container -->
@@ -14,14 +30,22 @@ include("includes/in_entete");
 		<?php include("includes/in_haut"); ?>
 
 		<div class="row">
+			<div class="three columns panel">
+			<h5>Les vols</h5>
+			<p>Voici la page où vous pouvez sélectionner le vol que vous allez prendre !</p>
+			<hr />
+			<p><a href="vols.php?restart=oui">Oubliez mes choix ! </a></p>
+			</div>
 			<div class="eight columns panel">
 
 <?php
 
-if(! isset($_POST['ville_depart'], $_POST['ville_arrivee']))
+/* si l'utilisateur n'a encore rien choisi */
+
+if(! isset($_SESSION['ville_depart']) && ! isset($_SESSION['ville_arrivee']))
 {
 	?>
-			<form class="nice" action="" method="POST" >
+			<form class="nice" action="vols.php" method="POST" >
 				<fieldset>
 				<h5>Choisissez la ville de départ</h5>
 				<p>Généralement c'est pas un vrai choix ça.</p>
@@ -48,10 +72,10 @@ if(! isset($_POST['ville_depart'], $_POST['ville_arrivee']))
 			</form>
 	<?php
 }
-else
+else if( ! isset($_SESSION['ville_arrivee']) )
 {
 	?>
-			<form class="nice" action="" method="POST" >
+			<form class="nice" action="vols.php" method="POST" >
 				<fieldset>
 				<h5>Choisissez la ville d'arrivée</h5>
 				<p>Généralement ça c'est un vrai choix.</p>
@@ -80,12 +104,39 @@ else
 			</form>
 	<?php
 }
-?>
-			</div>
-			<div class="three columns panel">
-			<h5>Les vols</h5>
-			<p>Voici la page où vous pouvez sélectionner le vol que vous allez prendre !</p>
+else if(! isset($_SESSION['date_depart']) && isset($_SESSION['ville_depart'], $_SESSION['ville_arrivee']))
+{
+	?>
+		<h5>Choisissez votre date et horraire de départ.</h5>
+		<form class="nice" action="vols.php" method="POST" >
+			<fieldset>
+			<select id="date_depart" name="date_depart">
+<?php 
 
+$query = "select V.idVol as idvol, V.idCompagnie as idcompagnie, C.nomCompagnie as nomcompagnie, 
+to_char(V.dateDepart, 'yyyy/mm/dd hh24:mi/ss') as ledatedepart,
+to_char(V.dateArrivee, 'yyyy/mm/dd hh24:mi/ss') as ledatearrivee 
+from VOL V JOIN COMPAGNIE C ON V.idCompagnie = C.idCompagnie
+where V.idVilleDepart=" . $_SESSION['ville_depart'] . 
+" and V.idVilleArrivee= " . $_SESSION['ville_arrivee'] . " and SYSDATE < V.dateDepart";
+
+$stmt = oci_parse($conn, $query);
+if(! oci_execute($stmt))
+	die("Il y a eu une erreur lors de la recherche des villes de départ");
+
+while($row = oci_fetch_assoc($stmt))
+{
+	echo "<option value='" . $row['IDVOL'] . "_" . $row['IDCOMPAGNIE'] . "' >" . 
+		$row['NOMCOMPAGNIE']. ". Date de départ : " .$row['LEDATEDEPART'] . " Date d'arrivée : " . $row['LEDATEARRIVEE'] . " </option>\n";
+}
+?>
+			</select>
+			<input type="submit" value="Go !" />
+		</form>
+
+<?php
+}
+?>
 			</div>
 		</div>
 	</div>
