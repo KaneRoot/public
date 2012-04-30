401e360e126bc4a2e6102acd051b66346c4e9c36
Philippe Pittoli <kane.root@gmail.com>
2012-04-30 16:47:04 +0200

    connexion presque sécurisée

--- ls5/bdd/projet/site/co_admin.php
+++ ls5/bdd/projet/site/co_admin.php
@@ -5,17 +5,19 @@
 include("co.php");
 
 /* si tout est bien rempli */
-if(	isset($_POST['c_login']) && strlen($_POST['c_login']) > 0 &&
-	isset($_POST['c_mdp']) && strlen($_POST['c_mdp']) > 0	)
+if(	isset($_POST['c_login'], $_POST['c_mdp']) && strlen($_POST['c_login']) > 0 &&
+	strlen($_POST['c_mdp']) > 0	)
 {
-	$_POST['c_login'] = "'" . $_POST['c_login'] . "'";
-	$_POST['c_mdp'] = "'" . $_POST['c_mdp'] . "'";
+	$login = $_POST['c_login'] ;
+	$mdp = $_POST['c_mdp'];
 
-	/* pas sécurisé */
-	$query = "select * from GESTIONNAIRE where loginGestionnaire=" . $_POST['c_login'] . 
-		"and mdpGestionnaire=" . $_POST['c_mdp'] ;
+	$query = "select * from GESTIONNAIRE 
+		where loginGestionnaire=:login and mdpGestionnaire=:mdp";
 
 	$stmt = oci_parse($conn, $query);
+	oci_bind_by_name($stmt, ':login', $login);
+	oci_bind_by_name($stmt, ':mdp', $mdp);
+
 	if( ! oci_execute($stmt))
 		die("Il y a eu une erreur à la connexion du client.");
 
@@ -23,7 +25,7 @@ if(	isset($_POST['c_login']) && strlen($_POST['c_login']) > 0 &&
 	{
 		/* Le type de connexion */
 		$_SESSION['connexion'] = "gestionnaire";
-		$_SESSION['login'] = $_POST['c_login'];
+		$_SESSION['login'] = $login;
 		header("Location: gestionnaire.php");
 	}
 	else
