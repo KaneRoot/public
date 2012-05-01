bb15c02f288ba27db33a4de6bc5b813e91eb1b20
Philippe Pittoli <kane.root@gmail.com>
2012-05-01 12:47:08 +0200

    toutes les procédures fonctionnent

--- ls5/bdd/projet/plsql/archivage.sql
+++ ls5/bdd/projet/plsql/archivage.sql
@@ -1,13 +1,13 @@
 -- procédure pour archiver les billets enregistrés
 create or replace procedure archivage
 IS
-	CURSOR curseur IS SELECT * FROM BILLET WHERE etatBillet='A';
+	CURSOR curseur IS SELECT * FROM BILLET WHERE etatBillet='A' and idBillet in (select idBillet from BILLET_CLIENT);
 	dateachat_v BILLET_CLIENT.dateAchat%TYPE;
 	idclient_v CLIENT.idClient%TYPE;
 BEGIN 
 	FOR ligne IN curseur
 	LOOP
-		select dateAchat,idClient into dateachat_v, idclient_v
+		select dateAchat, idClient into dateachat_v, idclient_v
 		from BILLET_CLIENT where idBillet=ligne.idBillet;
 
 		-- archives : idArchive, idVol, idCompagnie, prix, promo, dateAchat
@@ -16,5 +16,6 @@ BEGIN
 				ligne.prix, ligne.promo, dateachat_v, idclient_v);
 	END LOOP;
 END;
+/
 SHOW ERRORS procedure archivage;
 
