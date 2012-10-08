c0c2b18ec7e98d74bf685abdff92eed1ab419966
Philippe Pittoli <kane.root@gmail.com>
2012-10-09 00:05:43 +0200

    TP2 GLOO : -1 needed

--- ls6/gloo/tp2/tris/binaryinsertion.c
+++ ls6/gloo/tp2/tris/binaryinsertion.c
@@ -3,24 +3,25 @@ void BinInsertion(int * r, int lo, int up )
 {
 	int i, j, h, l;
 	int tempr;
-	// De 1 à n-1
-	for ( i = lo + 1 ; i <= up; i++ ) {
-		tempr = r[i]; // = r[1] à n-1
+	for ( i = lo + 1; i <= up ; i++ ) 
+	{
+		tempr = r[i];
 
-		l = lo;
+		l = lo - 1;
 		h = i;
-		// On recherche (de manière dichotomique) où placer l'élément courant
-		while( h-l > 1)
+		while ( h-l > 1 ) 
 		{
-			j = ( h + l ) / 2; // j = moyenne entre h et l
-			if ( tempr <= r[j] ) // Si l'élément du milieu est > à l'élément courant
+			j = ( h + l ) / 2;
+			if ( tempr <= r[j] ) 
 				h = j; 
 			else 
 				l = j;
 		}
-		// Si i > h alors on déplace de 1 vers la droite les éléments indice h à indice i
-		for ( j = i ; j > h ; j-- ) 
+		for ( j = i; j > h; j-- ) 
+		{
+
 			r[j] = r[j-1];
+		}
 		r[h] = tempr;
 	}
 }
