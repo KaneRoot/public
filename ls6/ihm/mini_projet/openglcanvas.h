a3b6ed2ee6aeea1626914c7d0bef2779d224128f
Philippe Pittoli <kane.root@gmail.com>
2012-10-24 09:48:29 +0200

    IHM TP4 : just started, compiles

--- ls6/ihm/mini_projet/openglcanvas.h
+++ ls6/ihm/mini_projet/openglcanvas.h
@@ -5,40 +5,17 @@
 
 class OpenGLCanvas: public wxGLCanvas {
 	public :
-		OpenGLCanvas::OpenGLCanvas(wxWindow *parent, wxWindowID id,
+		OpenGLCanvas(wxWindow *parent, wxWindowID id,
 			const wxPoint& pos, const wxSize& size, long style, const wxString& name);
-		OpenGLCanvas::~OpenGLCanvas(void);
+		~OpenGLCanvas(void);
 
 	private :
+		void Draw();
 		void OnPaint( wxPaintEvent& event );
 		void OnSize( wxSizeEvent& event );
 		void OnEraseBackground( wxEraseEvent& event );
 		DECLARE_EVENT_TABLE()
 };
-
-BEGIN_EVENT_TABLE(OpenGLCanvas, wxDialog)
-	EVT_PAINT(OpenGLCanvas::OnPaint)
-	EVT_SIZE(OpenGLCanvas::OnSize)
-	EVT_ERASE_BACKGROUND(OpenGLCanvas::OnEraseBackground)
-END_EVENT_TABLE ()
-
-OpenGLCanvas::OpenGLCanvas(wxWindow *parent, wxWindowID id,
-const wxPoint& pos, const wxSize& size, long style, const wxString& name): 
-wxGLCanvas(parent, id, pos, size, style, name)
-{
-}
-OpenGLCanvas::~OpenGLCanvas(void)
-{
-}
-void OnPaint( wxPaintEvent& event )
-{
-}
-void OnSize( wxSizeEvent& event )
-{
-}
-void OnEraseBackground( wxEraseEvent& event )
-{
-}
 
 #endif
 
