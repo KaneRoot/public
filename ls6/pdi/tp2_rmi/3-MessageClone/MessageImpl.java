1be873233a6dc1c2933f9bc87de666d7e472d51a
Philippe Pittoli <kane.root@gmail.com>
2012-10-24 21:47:53 +0200

    code plus propre

--- ls6/pdi/tp2_rmi/3-MessageClone/MessageImpl.java
+++ ls6/pdi/tp2_rmi/3-MessageClone/MessageImpl.java
@@ -3,40 +3,34 @@ import java.rmi.RemoteException ;
 import java.net.InetAddress.* ;
 import java.net.* ;
 
-public class MessageImpl 
-  extends UnicastRemoteObject
-  implements Message 
+public class MessageImpl extends UnicastRemoteObject implements Message 
 {
-  private String chaine = "l'original sur " ;
+	private String chaine = "l'original sur " ;
 
-  public MessageImpl ()
-    throws RemoteException 
-  {
-    super() ;
-  } ;
+	public MessageImpl () throws RemoteException 
+	{
+		super() ;
+	}
 
-  public MessageImpl (String ch)
-    throws RemoteException 
-  {
-    super() ;
-    chaine = ch ;
-  } ;
-    
-  public String messageDistant()
-    throws RemoteException
-  { 
-    String output=null;
-    try
-    {
-      output = chaine + (InetAddress.getLocalHost()).getHostName() ;
-    } 
-    catch (Exception e) {} ;
-    return(output) ;
-  }
+	public MessageImpl (String ch) throws RemoteException 
+	{
+		super() ;
+		chaine = ch ;
+	}
 
-  public Message clonage(String ch)
-    throws RemoteException
-  { 
-    return(new MessageImpl(ch)) ;
-  }
+	public String messageDistant() throws RemoteException
+	{ 
+		String output=null;
+		try
+		{
+			output = chaine + (InetAddress.getLocalHost()).getHostName() ;
+		} 
+		catch (Exception e) { System.out.println(e.getMessage()); } ;
+		return(output) ;
+	}
+
+	public Message clonage(String ch) throws RemoteException
+	{ 
+		return(new MessageImpl(ch)) ;
+	}
 }
