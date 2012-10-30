08dce9d11df8074591a6095c7e04a82e0d420d1d
Philippe Pittoli <kane.root@gmail.com>
2012-10-30 13:44:39 +0100

    SD : TP1 barely done

--- ls6/sd/1/exemple1.java
+++ ls6/sd/1/exemple1.java
@@ -1,4 +1,3 @@
-
 import visidia.simulation.process.algorithm.Algorithm;
 import visidia.simulation.process.edgestate.MarkedState;
 import visidia.simulation.process.messages.IntegerMessage;
@@ -8,27 +7,45 @@ import visidia.simulation.SimulationConstants.PropertyStatus;
 import visidia.simulation.*;
 import visidia.misc.*;
 
+import visidia.simulation.process.messages.Door;
 
 import java.awt.*;
 import javax.swing.*;
 
-public class exemple1 extends Algorithm {
+public class exemple1 extends Algorithm 
+{
+	public void init()
+	{
+		//System.out.print("\n");	
+		int nbNeighbors = getArity();
+		int id = getId();
+		int arity = getNetSize();
 
+		switch(id)
+		{
+			case 0 : 
+				System.out.print("Nombre de voisin du noeud  "+id+" = "+nbNeighbors+"\n");
+				System.out.print("Nombre de noeud dans le système "+arity+"\n");
+				putProperty("label","O"); 
+				sendAll(new StringMessage("Message"));
+				break;
+			default :
+				putProperty("label","T");//,SimulationConstants.PropertyStatus.DISPLAYED);
+				StringMessage message = (StringMessage) receive(new Door()); 
+				System.out.print("Message recu\n");
+				break;
+		}
+		/*
+		for (int i=0;i<nbNeighbors;i++)
+		{
+			StringMessage message = (StringMessage) receiveFrom(i);
+			System.out.print("Message recu\n");
+		}
+		*/
+	}
 
-
-public void init(){
-
-  
-
-    System.out.println("Mon premier exemple en Visidia.");
-  
-  
-                 
-
-} // fin init
-
- public Object clone(){
-	return new exemple1();
-    }
-    
-} //fin class exemple1
+	public Object clone()
+	{
+		return new exemple1();
+	}
+}
