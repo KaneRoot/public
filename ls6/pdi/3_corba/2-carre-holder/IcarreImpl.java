f95751622f0755a9202661d9f2e782bbe22639ed
Philippe Pittoli <kane.root@gmail.com>
2012-11-23 10:34:23 +0100

    rights changements

--- ls6/pdi/3_corba/2-carre-holder/IcarreImpl.java
+++ ls6/pdi/3_corba/2-carre-holder/IcarreImpl.java
@@ -2,9 +2,9 @@ import org.omg.CORBA.* ;
 
 public class IcarreImpl extends _IcarreImplBase
 {
-	public void carre (int source, LongHolder resultat)
+	public void carre (LongHolder source, LongHolder resultat)
 	{
 		System.out.println( "carre : requete recue pour le nombre : " + source ) ;
-		resultat.value = source * source ;
+		resultat.value = source.value * source.value ;
 	}
 }
